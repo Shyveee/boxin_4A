f296633368503d1814e4050f0f405c6b310a0581
Shyveee <shyveee@gmail.com>
2023-10-25 15:55:32 +0200

    Premier commit base

---
+++ src/main/java/com/example/projet4a_boxin/Projet4ABoxinApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet4a_boxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet4ABoxinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet4ABoxinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet4a_boxin/Projet4ABoxinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet4a_boxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet4ABoxinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
